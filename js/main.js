4af407aeda5477c5022b7ca90bd843b11fd0053a
purelyweb <49037603+purelyweb@users.noreply.github.com>
2021-05-05 22:31:39 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  speed: 900,
+	  asNavFor: '.slider-nav',
+	  initialSlide: 1
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  initialSlide: 1,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  arrows: false,
+	  centerMode: true,
+	  centerPadding: '20%',
+	  focusOnSelect: true,
+	  vertical: true,
+	  verticalSwiping: true,
+	  infinite: false,
+	  swipeToSlide: true,
+	  responsive: [
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1
+	      }
+	    }
+	   ]
+	});
+});
+
+
+$('.slider-nav').on('afterChange', function(slick, currentSlide){ 
+
+	var dataId = $('.slick-current').attr("data-slick-index"); // get only attr value
+    var slideBefore = dataId - 2;
+    var slideAfter = dataId + 2;
+
+    if(dataId <= 6){
+    	$(`.slider-nav .slick-slide[data-slick-index]`).css("opacity", "1");
+
+    	$(`.slider-nav .slick-slide[data-slick-index=${slideBefore}]`).css("opacity", "0.3");
+    	$(`.slider-nav .slick-slide[data-slick-index=${slideAfter}]`).css("opacity", "0.3");
+    }
+});
